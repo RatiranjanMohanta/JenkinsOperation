8e1bba4a8e384f4728e48bc5f7946ef55356728c
RatiranjanMohanta <mohanta.ratiranjan1@gmail.com>
2024-10-26 11:27:21 +0530

    commit2

--- jenkinsPractise/src/test/java/jenkinsPractise/JenkinsPractise.java
+++ jenkinsPractise/src/test/java/jenkinsPractise/JenkinsPractise.java
@@ -5,18 +5,18 @@ import org.testng.annotations.Test;
 public class JenkinsPractise {
 
 	
-	@Test
+	@Test(priority=1)
 	public void login() {
          System.out.println("Login");
 	}
 
 	
-	@Test
+	@Test(priority=2)
 	public void operation() {
 		 System.out.println("operation");
 	}
 
-	@Test
+	@Test(priority=3)
 	public void logout() {
 		 System.out.println("Logout");
 	}
