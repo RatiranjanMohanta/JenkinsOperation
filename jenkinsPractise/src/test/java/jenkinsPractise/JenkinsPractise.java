4a9b4167ebacfd171391982735bee0884703c992
RatiranjanMohanta <mohanta.ratiranjan1@gmail.com>
2024-10-26 16:27:01 +0530

    Added some code which will invoke browser

--- jenkinsPractise/src/test/java/jenkinsPractise/JenkinsPractise.java
+++ jenkinsPractise/src/test/java/jenkinsPractise/JenkinsPractise.java
@@ -1,13 +1,23 @@
 package jenkinsPractise;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class JenkinsPractise {
-
+     public static WebDriver driver;
 	
 	@Test(priority=1)
 	public void login() {
          System.out.println("Login");
+         driver=new ChromeDriver();
+         driver.get("https://freelance-learn-automation.vercel.app/login");
+ 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+ 		driver.manage().window().maximize();
+ 		driver.quit();
+         
 	}
 
 	
